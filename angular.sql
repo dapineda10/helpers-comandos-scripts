f49a71d7173ed7f39e05141dde020d8af6aef2d5
Danny Alejandro <50641273+dapineda10@users.noreply.github.com>
2022-03-19 23:10:36 -0500

    Create angular.sql

---
+++ angular.sql
@@ -0,0 +1,27 @@
+------------Terminal------------
+--Abrir terminal.
+Ctrl+Ñ
+--Para detener los procesos
+Ctrl+C
+--------------------------------
+
+------------Angular------------
+--Instalación de angular de forma global.
+npm install -g @angular/cli
+--Crear un nuevo proyecto.
+ng new nombre-proyecto
+--Ejecutar la aplicacion
+ng serve -o (ng serve --open)
+-------------------------------
+--------Instalar angular material-----------
+
+
+
+
+--Crear un nuevo componente en el sistema
+ng g c frm_macroruta/frm_macroruta
+--Crear un servicio de angular
+ng g s nomcarpetacreada/nombrecomponente/nombrecomponentedenuevo
+--Crear modulo
+ng g m nomcarpetacreada/nombremodulo --routing
+ng g m geoaseo/administracion/frm_microruta --routing
